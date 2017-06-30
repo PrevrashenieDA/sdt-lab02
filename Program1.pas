5b459fed5b7e07fbffbc46367f329df0e013eaa2
Степанов Богдан <Ibrabeat777@gmail.com>
2017-06-30 08:49:19 +0300

    Nova 4

--- Program1.pas
+++ Program1.pas
@@ -1,11 +1,16 @@
+Program Summa; 
 Const
   A = 23;
   B = 76;
 var
   S,U: integer;
+  D:real;
 begin
   S := A + B; 
   writeln('Сумма чисел 23 и 76 равна: S = ', S);
   U :=A * B;
-  writeln('Умножение чисел 23 и 76 равна: S = ', S);
+  writeln('Умножение чисел 23 и 76 равна: S = ', U);
+  D :=A/B;
+  writeln('Деление чисел 23 и 76 равна: S = ', D);
+ 
 end.
