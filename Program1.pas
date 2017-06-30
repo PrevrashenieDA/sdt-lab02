d108e65bd33b15f90e9382a07766d72930b23b28
Степанов Богдан <Ibrabeat777@gmail.com>
2017-06-30 08:48:33 +0300

    Nova 3

---
+++ Program1.pas
@@ -0,0 +1,11 @@
+Const
+  A = 23;
+  B = 76;
+var
+  S,U: integer;
+begin
+  S := A + B; 
+  writeln('Сумма чисел 23 и 76 равна: S = ', S);
+  U :=A * B;
+  writeln('Умножение чисел 23 и 76 равна: S = ', S);
+end.
